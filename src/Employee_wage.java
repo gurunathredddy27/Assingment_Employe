26e526241bbfa5b9a7096c2d62c3bff4cbf5a804
gurunathreddy2727@gmail.com <gurunathreddy2727@gmail.com>
2023-03-06 23:01:35 +0530

    Emp wage per month

--- src/Employee_wage.java
+++ src/Employee_wage.java
@@ -2,22 +2,13 @@
 public class Employee_wage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation!!");
-        int wagePerHr = 8;
-        int fullDayHr = 8;
-        int partTimeHr = 8;
-        int totalHour = 0;
-        int dailyWage = 0;
-        boolean partTime = true; // assuming the employee worked part-time
+        int totalWrkDay = 20;
+        int perHour = 20;
+        int perday = 8;
 
-        if (partTime) {
-            totalHour = partTimeHr;
-        } else {
-            totalHour = fullDayHr;
-        }
+        int wagePerMonth = totalWrkDay * perHour * perday;
+        System.out.println(wagePerMonth);
 
-        dailyWage = wagePerHr * totalHour;
-
-        System.out.println("The daily wage of the part-time employee is " + dailyWage);
 
     }
 }
