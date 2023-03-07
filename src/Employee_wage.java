fc6e13ccf43907d768ef76a0c9905ec5034def09
Gurunathreddy Tokala <119700685+gurunathredddy27@users.noreply.github.com>
2023-03-07 17:27:17 +0530

    Update Employee_wage.java

--- src/Employee_wage.java
+++ src/Employee_wage.java
@@ -2,11 +2,24 @@
 public class Employee_wage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation!!");
-        int wagePerHr = 8;
+        int ifpresent = (int)(Math.random()*2);  //uc1
+        if (ifpresent ==1){
+            System.out.println("Employee is present");
+        }else {
+            System.out.println("Employee is absent");
+        }
+
+        int wagePerHr = 20;         //uc2
+        int fulDay = 8;
+        int dailyWage = wagePerHr * fulDay;
+        System.out.println(dailyWage);
+
+
+        int ifWagePerHr = 8;   //uc3
         int fullDayHr = 8;
         int partTimeHr = 8;
         int totalHour = 0;
-        int dailyWage = 0;
+        int dailyWageIs = 0;
         boolean partTime = true; // assuming the employee worked part-time
 
         if (partTime) {
@@ -15,10 +28,20 @@ public class Employee_wage {
             totalHour = fullDayHr;
         }
 
-        dailyWage = wagePerHr * totalHour;
+        dailyWageIs = ifWagePerHr * totalHour;
 
-        System.out.println("The daily wage of the part-time employee is " + dailyWage);
+        System.out.println("The daily wage of the part-time employee is " + dailyWage);  //uc5
+        System.out.println("Welcome to Employee Wage Computation!!");
+        int totalWrkDay = 20;
+        int perHour = 20;
+        int perday = 8;
+
+        int wagePerMonth = totalWrkDay * perHour * perday;
+        System.out.println(wagePerMonth);
+
 
     }
 }
+
+
 
